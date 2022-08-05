3a9a3f7e89c3ed449812281eca4d4270e973ef2b
unique20021994 <unique20021994@gmail.com>
2022-08-05 11:25:43 +0300

    Update task-10.js

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -1,3 +1,37 @@
+'use strict';
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNum = document.querySelector('#controls>input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.getElementById("boxes");
+
+const createBoxes = amount => {
+	const elementsToAdd = []
+	for (let i = 0; i < amount; i++) {
+		const div = document.createElement('div')
+		div.style.height = `${30 + 10 * i}px`
+		div.style.width = `${30 + 10 * i}px`
+		div.style.background = getRandomHexColor()
+		elementsToAdd.push(div)
+	}
+	return elementsToAdd
+}
+
+const destroyBoxes = () => {
+	boxes.innerHTML = ''
+}
+
+btnCreate.addEventListener('click', () => {
+	let boxesToAdd = createBoxes(inputNum.value)
+	boxes.append(...boxesToAdd)
+})
+
+console.log(inputNum.value)
+
+btnDestroy.addEventListener('click', () => {
+	destroyBoxes.call()
+})
+
