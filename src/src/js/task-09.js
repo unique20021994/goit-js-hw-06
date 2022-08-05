846f4f52bb4c04a987d9f185d47c9b13c586dd49
unique20021994 <unique20021994@gmail.com>
2022-08-05 11:12:39 +0300

    Update task-09.js

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,15 @@
+'use strict';
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector("button.change-color");
+const body = document.querySelector('body');
+const span = document.querySelector('span.color');
+
+button.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+})
+
