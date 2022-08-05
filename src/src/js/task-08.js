b2962dcd66fbd79b7b45e4626a8c18c06f5a3d59
unique20021994 <unique20021994@gmail.com>
2022-08-05 10:43:13 +0300

    Update task-08.js

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,17 @@
+'use strict';
+
+const form = document.querySelector(".login-form");
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+event.preventDefault()
+const {elements: {email, password},
+} = event.currentTarget
+if (email.value === "" || password.value === ""){
+  return alert('Please fill in all the fields!')
+}
+const userInfo = {email: email.value, password: password.value};
+console.log(userInfo);
+event.currentTarget.reset();
+} 
 
